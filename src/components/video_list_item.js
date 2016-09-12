bf5a47e5ae17721ad768422fdb9eacc7fe25b394
adlawren <adlawren010@gmail.com>
2016-09-11 22:25:16 -0700

    Add support for video selection

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
-  return <li className="list-group-item">
-    <div className="video-list media">
-      <div className="media-left">
-        <img className="media-object" src={imageUrl} />
-      </div>
+  return (
+    <li
+      className="list-group-item"
+      onClick={() => onVideoSelect(video)}>
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
 
-      <div className="media-body">
-        <div className="media-heading">
-          {video.snippet.title}
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
         </div>
       </div>
-    </div>
-  </li>
+    </li>
+  );
 };
 
 export default VideoListItem;
