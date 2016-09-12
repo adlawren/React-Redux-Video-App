d736cead3573723a61c76cb24a0407e9b6875fb1
adlawren <adlawren010@gmail.com>
2016-09-11 22:48:38 -0700

    Add style, refactor, throttle video search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -18,24 +19,30 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: YOUTUBE_API_KEY, term: 'test' }, videos => {
-      this.setState({
-        videos,
-        selectedVideo: videos[0]
-      });
-    });
+    this.videoSearch('test');
   }
 
   render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
           videos={this.state.videos} />
       </div>
     );
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: YOUTUBE_API_KEY, term }, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
   }
 };
 
