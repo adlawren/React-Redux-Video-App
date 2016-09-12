d1e4d7d775e953fc5250aed5bb84f698118996da
adlawren <adlawren010@gmail.com>
2016-09-11 21:21:08 -0700

    Refactor app into class component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const YOUTUBE_API_KEY = 'AIzaSyDaOaFNHB6VQr-SYrfqpS9-CNpFqOuMmy4';
 
+YTSearch({ key: YOUTUBE_API_KEY, term: 'test'}, function(data) {
+  console.log(data);
+});
+
 // Create new, HTML producing component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 };
 
 // Take this components generated HTML, and put it on the page (in the DOM)
