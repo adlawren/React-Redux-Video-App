7ba61935915c749e9688c10a7878c6b120f17966
adlawren <adlawren010@gmail.com>
2016-09-11 21:26:14 -0700

    Add constructor

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import SearchBar from './components/search_bar';
 
 const YOUTUBE_API_KEY = 'AIzaSyDaOaFNHB6VQr-SYrfqpS9-CNpFqOuMmy4';
 
-YTSearch({ key: YOUTUBE_API_KEY, term: 'test'}, function(data) {
-  console.log(data);
-});
-
 // Create new, HTML producing component
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: YOUTUBE_API_KEY, term: 'test'}, videos => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
